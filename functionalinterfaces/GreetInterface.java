25b341c4e6131d44410fd18fefb9133df98bba94
Panktinew <130337976+Panktinew@users.noreply.github.com>
2023-10-26 12:42:44 +0530

    Add files via upload

---
+++ functionalinterfaces/GreetInterface.java
@@ -0,0 +1,12 @@
+//Program to demonstrate FunctionalInterface - Greet Interface
+//functional interface must has only one method.
+
+package com.tnsif.dayeight.interfaces.functionalinterfaces;
+
+@FunctionalInterface
+public interface GreetInterface {
+	
+	public String greet();
+	//public String greet1();
+
+}
